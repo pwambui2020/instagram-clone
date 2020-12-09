838810310b221b6f4b4542e89e3365044dc76eaa
pwambui2020 <paulynewambui2020@gmail.com>
2020-12-09 20:17:38 +0300

    Update tests.py

--- instaClone/tests.py
+++ instaClone/tests.py
@@ -23,7 +23,7 @@ class TestProfile(TestCase):
 class TestPost(TestCase):
     
     def setUp(self):
-        self.profile_test = Profile(name='Rustique', user=User(username='Waden'))
+        self.profile_test = Profile(name='Neema', user=User(username='Waden'))
         self.profile_test.save()
 
         self.image_test = Post(image='default.png', name='test', caption='default test', user=self.profile_test)
